DerpFest-Devices/device_samsung_a71

DerpFest Adaptation

AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a71.mk
+    $(LOCAL_DIR)/derp_a71.mk
+
+COMMON_LUNCH_CHOICES := \
+    derp_a71-eng \
+    derp_a71-user \
+    derp_a71-userdebug

DaViDev985/device_samsung_gts9wifi

AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_gts9wifi.mk
+    $(LOCAL_DIR)/derp_gts9wifi.mk
+
+COMMON_LUNCH_CHOICES := \
+    derp_gts9wifi-eng \
+    derp_gts9wifi-user \
+    derp_gts9wifi-userdebug