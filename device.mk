738b95801ca83e31f0da1f912d3123f0e054d9f6
DaViDev985 <davidev985@gmail.com>
2025-02-02 12:27:00 +0100

    gts9wifi: Add blueprint-based Google Apps debloat As this is a blueprint target, the makefile debloat previously used won't work. This works by adding a dummy .apk file (literally the default Android Studio project) that overrides all the packages you wish to remove.
    Future changes will include building the .apk during the build process (instead of including a presigned and prebuilt file) in order to publish the source files for the dummy app.

--- device.mk
+++ device.mk
@@ -40,6 +40,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# Remove Packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_wlanmdsp.otaupdate_symlink \
